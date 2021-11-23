3220d62d9bd996bdc503f0351c2a07971536e688
vgamas <valentin.gamas@outlook.com>
2021-11-22 20:37:59 -0500

    Actualizacion de ventas

--- Proyecto ciclo 4 - HTML/valida.js
+++ Proyecto ciclo 4 - HTML/valida.js
@@ -10,7 +10,8 @@ app.use(express.static(path.join(__dirname,'')));
 app.post('/validar',(req,res) => {
     const {c_usuario, c_contrasena} = req.body;
     
-//    console.log(c_usuario + "," + c_contrasena);
+    console.log(req.body);
+    console.log(c_usuario + "," + c_contrasena);
 
     if(c_usuario == "admininicial" && c_contrasena == "admin123456"){
         res.status(200).send('Usuario logueado correctamente')
@@ -21,17 +22,11 @@ app.post('/validar',(req,res) => {
 });
 
 app.post('/guardar_venta',(req,res) => {
-/*    const {c_usuario, c_contrasena} = req.body;
-    
-    console.log(c_usuario + "," + c_contrasena);
 
-    if(c_usuario == "admininicial" && c_contrasena == "admin123456"){
-        res.status(200).send('Usuario logueado correctamente')
-    }
-    else{
-        res.status(500).send('Usuario o clave incorrecta');
-    }*/
+    const {cedula_cliente} = req.body;
 
+    console.log(req.body);
+    console.log(cedula_cliente);
     // Hay que validar que los campos esten correctos
 
     res.status(200).send("Guardando la venta");
